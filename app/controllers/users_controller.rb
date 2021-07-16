255ee62fcf7dbf674d62e8ba4110e3e51a303695
taroken65 <yonemurano@gmail.com>
2021-07-16 02:30:11 +0000

    0716 commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,8 @@ class UsersController < ApplicationController
   def show
    @user = User.find(params[:id])
    @books = @user.books
+   @book = Book.new
   end
-  
-  
   
   def edit
    @user = User.find(params[:id])

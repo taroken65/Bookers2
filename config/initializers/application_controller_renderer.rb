395f4e6d869735e84affaaa421d01040107d122b
taroken65 <yonemurano@gmail.com>
2021-07-17 00:15:58 +0000

    0717 commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a2c0b938d1e38e10c0c6b1f78c51d4ca0dd9274dbc0659f15da8d55a049274b5f7ef01df50f7ccacfd4e1f9d3c69fdb98ddffa378eddf6ab974ebebb078c7434'
